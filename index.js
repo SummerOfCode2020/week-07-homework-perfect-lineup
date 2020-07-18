e5d57c4fa65dba8889487f2c14dcb023849297fc
Sharon Lin <hsiang.lin@gmail.com>
2020-07-18 00:12:34 -0400

    Almost. but for some reason not getting the right number of people from the same team.  tho at one point when I ran node, it worked, before I added the codes for gameId, will have to revisit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function validateLineup(lineup) {
-
   let validateResult = false
   let totalSalary = 0
   let countOfPosition = 0
@@ -9,91 +8,117 @@ function validateLineup(lineup) {
   let count2BPosition = 0
   let count3BPosition = 0
   let countSSPosition = 0
-  let countSameTeam = 0
+  let countSameTeam = 1
   let teamIDArray = []
   let sameTeamCriteriaMet = true
-  let highestDuplicate = 1
-  // lineup.forEach(player => {
-
-  // });
+  let highestDuplicateTeam = 1
+  let countSameGame = 1
+  let sameGameCriteriaMet = true
+  let highestDuplicateGame = 1
+  let gameIdArray = []
 
   lineup.forEach(player => {
     teamIDArray.push(player.teamId)
-  });
+  })
 
   for (let i = 0; i < teamIDArray.length; i++) {
     for (let j = 0; j < teamIDArray.length; j++) {
       if (i !== j && teamIDArray[i] === teamIDArray[j]) {
         countSameTeam++
 
-
-        if (countSameTeam > highestDuplicate) {
-          highestDuplicate = countSameTeam
+        if (countSameTeam > highestDuplicateTeam) {
+          highestDuplicateTeam = countSameTeam
         }
       }
       else {
         countSameTeam = 1
       }
-
     }
   }
 
-  if (highestDuplicate > 2) {
+  if (highestDuplicateTeam > 2) {
     sameTeamCriteriaMet = false
   }
 
-  console.log({ highestDuplicate })
-  console.log({ teamIDArray })
-  // teamIDArray.forEach(teamID => {
+  console.log({ highestDuplicateTeam })
 
-  // });
+  lineup.forEach(player => {
+    gameIdArray.push(player.gameId)
+  })
+
+  for (let i = 0; i < gameIdArray.length; i++) {
+    for (let j = 0; j < gameIdArray.length; j++) {
+      if (i !== j && gameIdArray[i] === gameIdArray[j]) {
+        countSameGame++
+
+        if (countSameGame > highestDuplicateGame) {
+          highestDuplicateGame = countSameGame
+        }
+      }
+      else {
+        countSameGame = 1
+      }
+    }
+  }
+
+  if (highestDuplicateGame > 3) {
+    sameGameCriteriaMet = false
+  }
+
+  //console.log({ highestDuplicateGame })
+  //console.log({ gameIdArray })
+
   lineup.forEach(player => {
     switch (player.position) {
       case 'OF':
         countOfPosition++
-        break;
+        break
       case 'P':
         countPPosition++
-        break;
+        break
       case 'C':
         countCPosition++
-        break;
+        break
       case '1B':
         count1BPosition++
-        break;
+        break
       case '2B':
         count2BPosition++
-        break;
+        break
       case '3B':
         count3BPosition++
-        break;
+        break
       case 'SS':
         countSSPosition++
-        break;
+        break
     }
   })
-  console.log(countOfPosition, countPPosition, countSSPosition, countCPosition)
+  //console.log(countOfPosition, countPPosition, countSSPosition, countCPosition)
 
   lineup.forEach(player => {
     totalSalary = totalSalary + player.salary
-  });
+  })
 
-  if (totalSalary < 45000 && countOfPosition === 3 && countPPosition === 1 && countCPosition === 1 && count1BPosition === 1 && count2BPosition === 1
-    && count3BPosition === 1 && countSSPosition === 1 && sameTeamCriteriaMet) {
+  if (totalSalary < 45000 && countOfPosition === 3 && countPPosition === 1 && countCPosition === 1 && count1BPosition === 1 && count2BPosition === 1 &&
+    count3BPosition === 1 && countSSPosition === 1 && sameTeamCriteriaMet && sameGameCriteriaMet) {
+
     validateResult = true
+
   } else {
+
     validateResult = false
   }
 
   return validateResult
+
 }
 
 const lineup2 = [{
-  id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+  id: 1, name: 'Chris Sale', position: 'P', teamId: 23, gameId: 123, salary: 9500
 }, {
-  id: 2, name: 'Yadier Molina', position: 'C', teamId: 17, gameId: 115, salary: 2500
+  id: 2, name: 'Yadier Molina', position: 'C', teamId: 12, gameId: 115, salary: 2500
 }, {
-  id: 3, name: 'Luke Voit', position: '1B', teamId: 15, gameId: 115, salary: 2800
+  id: 3, name: 'Mitch Moreland', position: '1B', teamId: 12, gameId: 123, salary: 2800
 }, {
   id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
 }, {
@@ -103,10 +128,30 @@ const lineup2 = [{
 }, {
   id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
 }, {
-  id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+  id: 8, name: 'Bryce Harper', position: 'OF', teamId: 12, gameId: 119, salary: 3800
 }, {
   id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
 }]
+
+// const lineup2 = [{
+//   id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+// }, {
+//   id: 2, name: 'Yadier Molina', position: 'C', teamId: 17, gameId: 115, salary: 2500
+// }, {
+//   id: 3, name: 'Luke Voit', position: '1B', teamId: 15, gameId: 115, salary: 2800
+// }, {
+//   id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+// }, {
+//   id: 5, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+// }, {
+//   id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+// }, {
+//   id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+// }, {
+//   id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+// }, {
+//   id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+// }]
 
 
 
@@ -118,5 +163,6 @@ const lineup2 = [{
 // return positionCriteria
 
 console.log(validateLineup(lineup2))
+
 
 module.exports = validateLineup
