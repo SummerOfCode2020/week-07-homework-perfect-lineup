126ef360b1ed18f4e00e3ea4cee13aaed5e3eb2d
Sharon Lin <hsiang.lin@gmail.com>
2020-07-17 23:31:26 -0400

    Added criteria of not having more than 2 players from a single team, passed node console.log

--- index.js
+++ index.js
@@ -11,17 +11,42 @@ function validateLineup(lineup) {
   let countSSPosition = 0
   let countSameTeam = 0
   let teamIDArray = []
-// lineup.forEach(player => {
-  
-// });
+  let sameTeamCriteriaMet = true
+  let highestDuplicate = 1
+  // lineup.forEach(player => {
 
-// lineup.forEach(player => {
-//   teamIDArray.push(player.teamId)
-// });  
+  // });
 
-// teamIDArray.forEach(teamID => {
-  
-// });
+  lineup.forEach(player => {
+    teamIDArray.push(player.teamId)
+  });
+
+  for (let i = 0; i < teamIDArray.length; i++) {
+    for (let j = 0; j < teamIDArray.length; j++) {
+      if (i !== j && teamIDArray[i] === teamIDArray[j]) {
+        countSameTeam++
+
+
+        if (countSameTeam > highestDuplicate) {
+          highestDuplicate = countSameTeam
+        }
+      }
+      else {
+        countSameTeam = 1
+      }
+
+    }
+  }
+
+  if (highestDuplicate > 2) {
+    sameTeamCriteriaMet = false
+  }
+
+  console.log({ highestDuplicate })
+  console.log({ teamIDArray })
+  // teamIDArray.forEach(teamID => {
+
+  // });
   lineup.forEach(player => {
     switch (player.position) {
       case 'OF':
@@ -54,7 +79,7 @@ function validateLineup(lineup) {
   });
 
   if (totalSalary < 45000 && countOfPosition === 3 && countPPosition === 1 && countCPosition === 1 && count1BPosition === 1 && count2BPosition === 1
-    && count3BPosition === 1 && countSSPosition === 1) {
+    && count3BPosition === 1 && countSSPosition === 1 && sameTeamCriteriaMet) {
     validateResult = true
   } else {
     validateResult = false
@@ -63,12 +88,12 @@ function validateLineup(lineup) {
   return validateResult
 }
 
-const lineup = [{
+const lineup2 = [{
   id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
 }, {
-  id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+  id: 2, name: 'Yadier Molina', position: 'C', teamId: 17, gameId: 115, salary: 2500
 }, {
-  id: 3, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+  id: 3, name: 'Luke Voit', position: '1B', teamId: 15, gameId: 115, salary: 2800
 }, {
   id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
 }, {
@@ -92,6 +117,6 @@ const lineup = [{
 // });
 // return positionCriteria
 
-console.log(validateLineup(lineup))
+console.log(validateLineup(lineup2))
 
-//module.exports = validateLineup
+module.exports = validateLineup
